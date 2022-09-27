09d7cb4183987ca264085bbf19b0a493cb89d4ce
Connor Lam <connorlam03@yahoo.com>
2022-09-27 16:13:15 -0700

    finished aws i think

---
+++ app/api/upload_routes.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request
+from app.models import db, Video, video
+from flask_login import current_user, login_required
+from app.s3_helpers import (
+    upload_file_to_s3, allowed_file, get_unique_filename)
+
+upload_routes = Blueprint("upload", __name__)
+
+
+@upload_routes.route("", methods=["POST"])
+@login_required
+def upload_video():
+    if "video" not in request.files:
+        return {"errors": "video required"}, 400
+
+    video = request.files["video"]
+
+    if not allowed_file(video.filename):
+        return {"errors": "file type not permitted"}, 400
+    
+    video.filename = get_unique_filename(video.filename)
+
+    upload = upload_file_to_s3(video)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    new_video = Video(user=current_user, url=url)
+    db.session.add(new_video)
+    db.session.commit()
+    return {"url": url}
---
+++ app/models/video.py
@@ -0,0 +1,27 @@
+from .db import db
+from sqlalchemy.sql import func
+
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    title = db.Column(db.String, nullable = False)
+    video = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = False)
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    user = db.relationship('User', back_populates='videos')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "ownderId": self.owner_id,
+            "title" : self.title,
+            "video" : self.video,
+            "description" : self.description,
+            "time_created" : self.time_created,
+            "time_updated" : self.time_updated
+        }
